6e8b0c8e513cffee1baf9572b9c9f3475d155a96
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-05-12 12:27:25 +0200

    refactoring typos

---
+++ src/main/java/com/example/auta/domain/repositories/CompanyRepository.java
@@ -0,0 +1,1 @@
+package com.example.auta.domain.repositories;import com.example.auta.domain.entities.CompanyEntity;import org.springframework.data.repository.PagingAndSortingRepository;import org.springframework.stereotype.Repository;import java.util.UUID;@Repositorypublic interface CompanyRepository extends PagingAndSortingRepository<CompanyEntity, UUID> {}
--- src/main/java/com/example/auta/domain/repositories/CustomerRepositories.java
+++
@@ -1,1 +0,0 @@
-package com.example.auta.domain.repositories;import com.example.auta.domain.entities.CustomerEntity;import org.springframework.data.repository.PagingAndSortingRepository;import org.springframework.stereotype.Repository;import java.util.UUID;@Repositorypublic interface CustomerRepositories extends PagingAndSortingRepository<CustomerEntity, UUID> {}
