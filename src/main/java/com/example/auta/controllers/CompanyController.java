2173564047e617b9b719d51f2b87e6b79c83e29d
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-05-12 12:27:25 +0200

    refactoring typos
    (cherry picked from commit 6e8b0c8e513cffee1baf9572b9c9f3475d155a96)

--- src/main/java/com/example/auta/controllers/CompanyController.java
+++ src/main/java/com/example/auta/controllers/CompanyController.java
@@ -11,7 +11,7 @@ import java.util.UUID;
 
 
 @RestController
-@RequestMapping("/Company")
+@RequestMapping("/company")
 public class CompanyController {
 
 
--- src/main/java/com/example/auta/services/CompanyService.java
+++ src/main/java/com/example/auta/services/CompanyService.java
@@ -3,12 +3,10 @@ package com.example.auta.services;
 import com.example.auta.domain.entities.BranchEntity;
 import com.example.auta.domain.entities.CompanyEntity;
 import com.example.auta.domain.repositories.BranchRepository;
-import com.example.auta.domain.repositories.CompanyRepositories;
+import com.example.auta.domain.repositories.CompanyRepository;
 import com.example.auta.models.classes.Branch;
 import com.example.auta.models.classes.Company;
-import com.example.auta.models.classes.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -19,28 +17,28 @@ import java.util.UUID;
 @Service
 public class CompanyService {
 
-    private final CompanyRepositories companyRepositories;
+    private final CompanyRepository companyRepository;
     private final BranchRepository branchRepository;
 
     @Autowired
-    public CompanyService(CompanyRepositories companyRepo, BranchRepository branchRepo){
-        this.companyRepositories = companyRepo;
+    public CompanyService(CompanyRepository companyRepo, BranchRepository branchRepo){
+        this.companyRepository = companyRepo;
         this.branchRepository = branchRepo;
     }
     public Map<UUID, Company> getCompany() {
 
         Map<UUID,Company> map = new HashMap<>();
-        companyRepositories.findAll().forEach(
+        companyRepository.findAll().forEach(
                 element -> map.put(element.getId(),map(element)));
         return map;
     }
 
     public UUID addCompany(Company company) {
-        return companyRepositories.save(map(company)).getId();
+        return companyRepository.save(map(company)).getId();
     }
 
     public boolean updateCompany(UUID uuid, Company company) {
-        Optional<CompanyEntity> updateCompany = companyRepositories.findById(uuid);
+        Optional<CompanyEntity> updateCompany = companyRepository.findById(uuid);
         if (updateCompany.isPresent()) {
             updateCompany.get().setName(company.getName());
             updateCompany.get().setOwner(company.getOwner());
@@ -48,7 +46,7 @@ public class CompanyService {
             updateCompany.get().setBranches(company.getBranches());
             updateCompany.get().setDomain(company.getDomain());
             updateCompany.get().setLogotype(company.getLogotype());
-            companyRepositories.save(updateCompany.get());
+            companyRepository.save(updateCompany.get());
             return true;
         } else {
             return false;
@@ -57,14 +55,14 @@ public class CompanyService {
     }
 
     public boolean deleteBranch(UUID companyUUID, UUID branchUUID) throws Exception {
-        CompanyEntity company = companyRepositories
+        CompanyEntity company = companyRepository
                 .findById(companyUUID)
                 .orElseThrow(Exception::new);
         BranchEntity branch = branchRepository
                 .findById(branchUUID)
                 .orElseThrow(Exception::new);
         if (company.getBranches().remove(branch)){
-            companyRepositories.save(company);
+            companyRepository.save(company);
             return true;
         } else {
             return false;
@@ -73,8 +71,8 @@ public class CompanyService {
 
     public boolean deleteCompany(UUID uuid) {
 
-        if (companyRepositories.existsById(uuid)) {
-            companyRepositories.deleteById(uuid);
+        if (companyRepository.existsById(uuid)) {
+            companyRepository.deleteById(uuid);
             return true;
         } else {
             return false;
