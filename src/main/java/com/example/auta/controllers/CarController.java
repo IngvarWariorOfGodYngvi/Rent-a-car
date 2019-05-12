689ad3587efab417845e95320abc084aa87b8d3e
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2019-05-12 13:41:04 +0200

    car service and controllers. methods and others but not all

--- src/main/java/com/example/auta/controllers/CarController.java
+++ src/main/java/com/example/auta/controllers/CarController.java
@@ -1,37 +1,31 @@
 package com.example.auta.controllers;
 
 import com.example.auta.models.classes.Car;
+import com.example.auta.models.classes.Customer;
 import com.example.auta.services.CarService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/car")
+@RequestMapping("/Car")
 public class CarController {
 
 
     @Autowired
     private CarService carService;
 
-    @PostMapping("/addcar")
-    public UUID addCar(@RequestBody Car car) {
-        return carService.addCar(car);
-    }
-
-    @DeleteMapping("/deletecar/{uuid}")
-    public boolean deleteCar(@PathVariable UUID uuid) {
-        return carService.deleteCar(uuid);
-    }
+    @PostMapping("/carlist")
+    public Map<UUID, Car> getCars(){ return carService.getCars();}
 
     @PostMapping("/updatecarstatus/{uuid}")
     public boolean updateCarStatus(@PathVariable UUID uuid) {
         return carService.updateCarStatus(uuid);
-
     }
-    @PostMapping("/updatecar/{uuid}")
-    public UUID updateCarMileage(@PathVariable UUID uuid){
+    @PostMapping("/updatecarmileage/{uuid}")
+    public boolean updateCarMileage(@PathVariable UUID uuid){
         return carService.updateCarMileage(uuid);
     }
 
--- src/main/java/com/example/auta/services/CarService.java
+++ src/main/java/com/example/auta/services/CarService.java
@@ -1,10 +1,18 @@
 package com.example.auta.services;
 
+import com.example.auta.domain.entities.CarEntity;
+import com.example.auta.domain.entities.CompanyEntity;
 import com.example.auta.domain.repositories.CarRepository;
 import com.example.auta.models.classes.Car;
+import com.example.auta.models.classes.Company;
+import com.example.auta.models.enums.CarStatus;
+import com.example.auta.models.enums.Suspension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 @Service
@@ -18,18 +26,62 @@ public class CarService {
     }
 
     public UUID addCar(Car car) {
-        return null;
+        return carRepository.save(map(car)).getId();
     }
 
     public boolean deleteCar(UUID uuid) {
-        return false;
+
+        if (carRepository.existsById(uuid)) {
+            carRepository.deleteById(uuid);
+            return true;
+        } else {
+            return false;
+        }
+    }
+    public Map<UUID,Car> getCars(){
+        Map<UUID,Car> map = new HashMap<>();
+        carRepository.findAll().forEach(
+                element -> map.put(element.getId(),map(element)));
+        return map;
     }
 
     public boolean updateCarStatus(UUID uuid) {
         return false;
     }
 
-    public UUID updateCarMileage(UUID uuid) {
-        return null;
+    public boolean updateCarMileage(UUID carUuid) {
+
+        return false;
     }
+
+
+    private CarEntity map(Car c) {
+        CarEntity cc = new CarEntity();
+
+        return cc.builder()
+                .make(c.getMake())
+                .model(c.getModel())
+                .suspension(c.getSuspension())
+                .yearOfProduction(c.getYearOfProduction())
+                .color(c.getColor())
+                .mileage(c.getMileage())
+                .carStatus(c.getCarStatus())
+                .dailyPrice(c.getDailyPrice())
+                .build();
+    }
+
+
+    private Car map(CarEntity c) {
+        return new Car().builder()
+                .make(c.getMake())
+                .model(c.getModel())
+                .suspension(c.getSuspension())
+                .yearOfProduction(c.getYearOfProduction())
+                .color(c.getColor())
+                .mileage(c.getMileage())
+                .carStatus(c.getCarStatus())
+                .dailyPrice(c.getDailyPrice())
+                .build();
+    }
+
 }
