e51b73bb22802ab3b7be78c0bcd26d28548dfc0f
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2019-05-14 15:34:22 +0200

    adding getEmployee method in BranchController
    (cherry picked from commit b1e6808356db81a0d4f44d40697679ed1c2a9ea4)

--- src/main/java/com/example/auta/controllers/BranchController.java
+++ src/main/java/com/example/auta/controllers/BranchController.java
@@ -1,11 +1,14 @@
 package com.example.auta.controllers;
 
 import com.example.auta.models.classes.Car;
+import com.example.auta.models.classes.Employee;
 import com.example.auta.services.BranchService;
 import com.example.auta.services.CarService;
+import com.example.auta.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
 import java.util.UUID;
 
 @RestController
@@ -16,6 +19,8 @@ public class BranchController {
     private BranchService branchService;
     @Autowired
     private CarService carService;
+    @Autowired
+    private EmployeeService employeeService;
 
 
     @PostMapping("/{branchUUID}/addcar")
@@ -28,4 +33,6 @@ public class BranchController {
                              @PathVariable UUID carUUID) throws Exception {
         return carService.deleteCar(branchUUID,carUUID);
     }
+    @PostMapping("{branchUUID}/employeelist")
+    public Map<UUID, Employee> getEmployee() {return employeeService.getEmployees();}
 }
