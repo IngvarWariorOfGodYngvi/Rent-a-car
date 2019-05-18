7736e3167b70be1c7d2ccb22235f9e90cfaed7c2
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-05-18 11:47:27 +0200

    added getEmployeesTest in BranchControllerTest

--- src/test/java/com/example/auta/controllers/BranchControllerTest.java
+++ src/test/java/com/example/auta/controllers/BranchControllerTest.java
@@ -3,10 +3,14 @@ package com.example.auta.controllers;
 import com.example.auta.domain.entities.BranchEntity;
 import com.example.auta.domain.entities.CarEntity;
 import com.example.auta.domain.entities.CompanyEntity;
+import com.example.auta.domain.entities.EmployeeEntity;
 import com.example.auta.domain.repositories.BranchRepository;
 import com.example.auta.domain.repositories.CarRepository;
 import com.example.auta.domain.repositories.CompanyRepository;
+import com.example.auta.domain.repositories.EmployeeRepository;
 import com.example.auta.models.classes.Car;
+import com.example.auta.models.classes.Company;
+import com.example.auta.models.classes.Employee;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -14,10 +18,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.Map;
 import java.util.UUID;
 
 import static org.junit.Assert.*;
@@ -35,6 +43,8 @@ public class BranchControllerTest {
     @Autowired
     private BranchRepository branchRepository;
 
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @Autowired
     private CompanyRepository companyRepository;
@@ -77,5 +87,21 @@ public class BranchControllerTest {
                                               branchEntity.getId().toString(),
                                               carEntity.getId().toString()));
         assertTrue(!carRepository.findById(carEntity.getId()).isPresent());
+    }
+
+    @Test
+    public void getEmployee() {
+        EmployeeEntity employee1 = EmployeeEntity.builder().surname("Kwiatkowski").branch(branchEntity).build();
+        EmployeeEntity employee2 = EmployeeEntity.builder().surname("Zielinski").branch(branchEntity).build();
+        Arrays.asList(employee1, employee2).forEach(employeeRepository::saveAndFlush);
+        String branchUUID = branchEntity.getId().toString();
+        ResponseEntity<Map<UUID, Employee>> re = testRestTemplate
+                .exchange(String.format("/branch/%s/employeelist", branchUUID),
+                          HttpMethod.GET,
+                          null,
+                          new ParameterizedTypeReference<Map<UUID, Employee>>(){});
+        Map<UUID, Employee> employeeMap = re.getBody();
+        assertEquals(HttpStatus.OK, re.getStatusCode());
+        assertEquals(2, employeeMap.size());
     }
 }
