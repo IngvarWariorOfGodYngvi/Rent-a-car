18a3f838896241d3d00740f12989a9bc72117551
Wojciech Oczkowski <wojciech.oczkowski@gmail.com>
2019-05-19 00:46:18 +0200

    added addTest implementation and other needed changes

--- src/main/java/com/example/auta/services/CustomerService.java
+++ src/main/java/com/example/auta/services/CustomerService.java
@@ -1,18 +1,11 @@
 package com.example.auta.services;
 
-import com.example.auta.domain.entities.CompanyEntity;
 import com.example.auta.domain.entities.CustomerEntity;
-import com.example.auta.domain.repositories.CompanyRepository;
 import com.example.auta.domain.repositories.CustomerRepository;
-import com.example.auta.domain.repositories.ReservationRepository;
 import com.example.auta.models.classes.Customer;
-import com.example.auta.models.classes.Reservation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.support.SimpleJpaRepository;
 import org.springframework.stereotype.Service;
 
-import javax.persistence.EntityNotFoundException;
 import java.util.*;
 import javax.persistence.EntityNotFoundException;
 import java.util.HashMap;
@@ -27,7 +20,7 @@ public class CustomerService {
     private final CustomerRepository customerRepository;
 
     public UUID addCustomer(Customer customer) {
-        return customerRepository.save(map(customer)).getId();
+        return customerRepository.saveAndFlush(map(customer)).getId();
     }
 
     public Customer readCustomer(CustomerEntity customer) {
@@ -36,7 +29,7 @@ public class CustomerService {
 
     public CustomerEntity getOrCreateCustomerEntity(Customer customer) {
         Optional<CustomerEntity> customerEntity = customerRepository
-                .findCustomerEntityByFornameEqualsAndLastnameEqualsAndEmailEquals(
+                .findCustomerEntityByForenameEqualsAndSurnameEqualsAndEmailEquals(
                         customer.getForename(), customer.getSurname(), customer.getEmail());
         return customerEntity.orElse(customerRepository.saveAndFlush(map(customer)));
     }
@@ -56,10 +49,10 @@ public class CustomerService {
                     .findById(customerUUID)
                     .orElseThrow(EntityNotFoundException::new);
             if (customer.getForename() != null) {
-                updateCustomerEntity.setForname(customer.getForename());
+                updateCustomerEntity.setForename(customer.getForename());
             }
             if (customer.getSurname() != null) {
-                updateCustomerEntity.setLastname(customer.getSurname());
+                updateCustomerEntity.setSurname(customer.getSurname());
             }
             if (customer.getAddress() != null) {
                 updateCustomerEntity.setAddress(customer.getAddress());
@@ -79,8 +72,8 @@ public class CustomerService {
         return Customer.builder()
                 .address(source.getAddress())
                 .email(source.getEmail())
-                .forename(source.getForname())
-                .surname(source.getLastname())
+                .forename(source.getForename())
+                .surname(source.getSurname())
                 .build();
     }
 
@@ -89,8 +82,8 @@ public class CustomerService {
         return CustomerEntity.builder()
                 .address(source.getAddress())
                 .email(source.getEmail())
-                .forname(source.getForename())
-                .lastname(source.getSurname())
+                .forename(source.getForename())
+                .surname(source.getSurname())
                 .build();
     }
 }
