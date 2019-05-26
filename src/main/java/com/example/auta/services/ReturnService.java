e0b19616ae0dec1320d62d8de71ecf4ca583993b
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2019-05-26 10:44:37 +0200

    change add return methods in return service

--- src/main/java/com/example/auta/services/ReturnService.java
+++ src/main/java/com/example/auta/services/ReturnService.java
@@ -1,8 +1,10 @@
 package com.example.auta.services;
 
+import com.example.auta.domain.entities.BranchEntity;
 import com.example.auta.domain.entities.EmployeeEntity;
 import com.example.auta.domain.entities.RentEntity;
 import com.example.auta.domain.entities.ReturnEntity;
+import com.example.auta.domain.repositories.BranchRepository;
 import com.example.auta.domain.repositories.EmployeeRepository;
 import com.example.auta.domain.repositories.RentRepository;
 import com.example.auta.domain.repositories.ReturnRepository;
@@ -10,6 +12,8 @@ import com.example.auta.models.classes.Return;
 import com.example.auta.models.enums.ReservationStatus;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import static java.time.temporal.ChronoUnit.DAYS;
 
 import javax.persistence.EntityExistsException;
 import javax.persistence.EntityNotFoundException;
@@ -25,6 +29,7 @@ public class ReturnService {
     private final ReturnRepository returnRepository;
     private final EmployeeRepository employeeRepository;
     private final RentRepository rentRepository;
+    private final BranchRepository branchRepository;
 
     public Map<UUID, Return> getReturns() {
         return null;
@@ -49,19 +54,31 @@ public class ReturnService {
         return true;
     }
 
-    public UUID addReturn(UUID employeeUUID, UUID rentUUID, Return ret) {
+    public UUID addReturn(UUID employeeUUID, UUID rentUUID, Return ret, UUID branchUUID) {
         EmployeeEntity employeeEntity = employeeRepository
                 .findById(employeeUUID)
                 .orElseThrow(EntityNotFoundException::new);
         RentEntity rentEntity = rentRepository
                 .findById(rentUUID)
                 .orElseThrow(EntityNotFoundException::new);
+        Long daysDif = DAYS.between(rentEntity.getReservation().getRentalEndDate(),ret.getRentalEndDate());
+
+        if(daysDif>0){
+            ret.setExtraPayment(ret.getReservation().getCar().getDailyPrice().multiply(new BigDecimal(daysDif)).multiply(new BigDecimal(1.1)));
+        }
+        BranchEntity branchEntity = branchRepository
+                .findById(branchUUID)
+                .orElseThrow(EntityNotFoundException::new);
+        if(!rentEntity.getReservation().getReturnBranch().equals(branchEntity)){
+         ret.setExtraPayment(ret.getExtraPayment().add(new BigDecimal(50)));
+        }
         ReturnEntity returnEntity =  ReturnEntity.builder()
                 .employee(employeeEntity)
                 .reservation(rentEntity.getReservation())
                 .rentalEndDate(LocalDate.now())
                 .endMileage(ret.getEndMileage())
                 .build();
+
         rentEntity.getReservation().getCar().setMileage(ret.getEndMileage());
         rentEntity.getReservation().setReservationStatus(ReservationStatus.RETURNED);
         return returnRepository.saveAndFlush(returnEntity).getId();
